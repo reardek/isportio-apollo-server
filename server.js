78741c65f2cce9d7c288a4d3be90072e8a6f1298
reardek <jjkowalinski@yahoo.com>
2020-08-23 22:07:21 +0200

    Add tracing

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require("apollo-server-express");
 const mongoose = require("./config/database");
 const typeDefs = require("./gql/schemas");
 const resolvers = require("./gql/resolvers");
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers, tracing: true });
 const app = express();
 
 server.applyMiddleware({ app });
