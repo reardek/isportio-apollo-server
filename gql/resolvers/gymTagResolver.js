cada52d3da4ba174d7c7e50d1debe7195f9a97e0
reardek <rearden@DESKTOP-0DE631R.localdomain>
2020-08-25 19:31:25 +0200

    Remove Description field from GymTag

--- gql/resolvers/gymTagResolver.js
+++ gql/resolvers/gymTagResolver.js
@@ -6,7 +6,6 @@ module.exports = {
     addGym: (parent, gymTag) => {
       const newGymTag = new GymTag({
         name: gymTag.name,
-        description: gymTag.description,
       });
       return newGymTag.save();
     },
--- mongooseSchema/gymTag.js
+++ mongooseSchema/gymTag.js
@@ -2,7 +2,6 @@ const { Schema, model } = require("mongoose");
 
 const gymSchema = Schema({
   name: String,
-  description: String,
 });
 
 module.exports = model("gymTag", gymSchema);
