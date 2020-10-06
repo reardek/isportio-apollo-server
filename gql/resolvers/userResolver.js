84b40d5a093048309144c13aa555d6696084a679
reardek <rearden@DESKTOP-0DE631R.localdomain>
2020-10-06 19:56:08 +0200

    Mutation login that return jwt

--- gql/resolvers/userResolver.js
+++ gql/resolvers/userResolver.js
@@ -1,5 +1,9 @@
 const User = require("../../mongooseSchema/user");
 const bcrypt = require("bcrypt");
+const fs = require("fs");
+const path = require("path");
+const jwt = require("jsonwebtoken");
+const { join } = require("path");
 
 module.exports = {
   Query: {
@@ -18,9 +22,32 @@ module.exports = {
         password: pass,
         firstName: user.firstName,
         lastName: user.lastName,
+        role: user.role,
         birthDate: user.birthDate,
       });
       return newUser.save();
+    },
+    loginUser: async (parent, { loginEmail, password }) => {
+      const findUser = await User.findOne(
+        { loginEmail: loginEmail },
+        "loginEmail password role"
+      );
+
+      const validate = await bcrypt.compare(password, findUser.password);
+      if (validate) {
+        const pathToKey = path.join(__dirname, "..", "..", "private.key");
+        const key = await fs.readFileSync(pathToKey, "utf-8");
+        const jsonToken = await jwt.sign(
+          {
+            loginEmail: findUser.loginEmail,
+            role: findUser.role,
+            exp: Math.floor(Date.now() / 1000) + 60 * 60,
+          },
+          key,
+          { algorithm: "RS256" }
+        );
+        return jsonToken;
+      }
     },
   },
 };
